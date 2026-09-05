148f24580d3fd26d68640f44112c6a66f4c97a18
Jacob Butt <53582466+JacButt@users.noreply.github.com>
2026-09-05 22:08:47 +0100

    Add files via upload

--- Scripts/Tables_Initialisation.sql
+++ Scripts/Tables_Initialisation.sql
@@ -20,17 +20,19 @@ USE ProjectDataWarehouse;
 GO
 
 
+
+
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 
 CREATE TABLE bronze.crm_cust_info (
-	cst_id INT,
-	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
-	cst_marital_status NVARCHAR(50),
-	cst_gndr NVARCHAR(50),
-	cst_create_date DATE
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE
 );
 
 
@@ -38,13 +40,13 @@ IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info (
-	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT,
-	prd_line NVARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE
+	prd_id				INT,
+	prd_key				NVARCHAR(50),
+	prd_nm				NVARCHAR(50),
+	prd_cost			INT,
+	prd_line			NVARCHAR(50),
+	prd_start_dt		DATETIME,
+	prd_end_dt			DATETIME
 );
 
 
@@ -52,15 +54,15 @@ IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt DATE,
-	sls_ship_dt DATE,
-	sls_due_dt DATE,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
+	sls_ord_num			NVARCHAR(50),
+	sls_prd_key			NVARCHAR(50),
+	sls_cust_id			INT,
+	sls_order_dt		INT,
+	sls_ship_dt			INT,
+	sls_due_dt			INT,
+	sls_sales			INT,
+	sls_quantity		INT,
+	sls_price			INT
 );
 
 
@@ -68,9 +70,9 @@ IF OBJECT_ID ('bronze.erp_CUST_AZ12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_CUST_AZ12;
 
 CREATE TABLE bronze.erp_CUST_AZ12 (
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50)
+	CID					NVARCHAR(50),
+	BDATE				DATE,
+	GEN					NVARCHAR(50)
 );
 
 
@@ -78,8 +80,8 @@ IF OBJECT_ID ('bronze.erp_LOC_A101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_LOC_A101;
 
 CREATE TABLE bronze.erp_LOC_A101(
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50)
+	CID					NVARCHAR(50),
+	CNTRY				NVARCHAR(50)
 );
 
 
@@ -87,8 +89,8 @@ IF OBJECT_ID ('bronze.erp_PX_CAT_G1V2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_PX_CAT_G1V2;
 
 CREATE TABLE bronze.erp_PX_CAT_G1V2 (
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50)
+	ID					NVARCHAR(50),
+	CAT					NVARCHAR(50),
+	SUBCAT				NVARCHAR(50),
+	MAINTENANCE			NVARCHAR(50)
 );
