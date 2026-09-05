709a326db3f6a702cac1198470c5bde945f76867
Jacob Butt <53582466+JacButt@users.noreply.github.com>
2026-09-05 18:11:09 +0100

    Add files via upload

--- Scripts/DWH_Initialisation.sql
+++ Scripts/DWH_Initialisation.sql
@@ -1,20 +1,50 @@
-------------------
---- Creating empty Data Warehouse
-------------------
+/*
+-------------------------------------------------
+For Initialisation of Database and Schemas
+-------------------------------------------------
+
+This script is used to create a new database titled 'ProjectDataWarehouse' after checking if it already exists.
+If the database does already exist, it is dropped and recreated.
+After this, the script creates three schemas inside the database, titled 'bronze', 'silver', and 'gold'
+
+WARNING:
+
+	Running this script will drop the entirety of 'ProjectDataWarehouse' if it alrwady exists.
+	Take caution when executing and ensure that any existing data is properly backed up beforehand.
+
+*/
 
 USE master;
+GO
+
+--------------------------------
+--- Checking for existence of current Database
+--------------------------------
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'ProjectDataWarehouse')
+BEGIN
+	ALTER DATABASE ProjectDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE ProjectDataWarehouse;
+END;
+GO
+
+-------------------------------
+--- Creating the Database ProjectDataWarehouse
+-------------------------------
 
 CREATE DATABASE ProjectDataWarehouse;
 
 USE ProjectDataWarehouse;
 
-----------------------
---- Creating schema for each planned layer
-----------------------
+----------------------------------
+--- Creating schemas for each planned layer in the Data Warehouse
+----------------------------------
 
 CREATE SCHEMA bronze;
 GO
+
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
 GO
